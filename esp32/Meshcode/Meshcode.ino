5cb5c469603e1a2768832bbbf4241f7507fa7a84
SjoerdFroma <97604829+SjoerdFroma@users.noreply.github.com>
2023-02-07 13:06:01 +0100

    Create Meshcode.ino

---
+++ esp32/Meshcode/Meshcode.ino
@@ -0,0 +1,125 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include "painlessMesh.h"
+#include <Arduino_JSON.h>
+
+// MESH Details
+#define   MESH_PREFIX     "ITDirect" 
+#define   MESH_PASSWORD   "ITDirect" 
+#define   MESH_PORT       5555 
+
+//BME object on the default I2C pins
+Adafruit_BME280 bme;
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+//Number for this node
+int nodeNumber = 1;
+
+//String to send to other nodes with sensor readings
+String readings;
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+String getReadings(); // Prototype for sending sensor readings
+
+//Create tasks: to send messages and get readings;
+Task taskSendMessage(TASK_SECOND * 5 , TASK_FOREVER, &sendMessage);
+
+String getReadings () {
+  JSONVar jsonReadings;
+  jsonReadings["node"] = nodeNumber;
+  jsonReadings["temp"] = bme.readTemperature();
+  jsonReadings["hum"] = bme.readHumidity();
+  jsonReadings["pres"] = bme.readPressure()/100.0F;
+    // Add the potentiometer value
+  int analogValue = analogRead(4);
+  float voltage = floatMap(analogValue, 0, 4095, 0, 3.3);
+  jsonReadings["analog"] = analogValue;
+  jsonReadings["volt"] = analogValue;
+  readings = JSON.stringify(jsonReadings);
+  return readings;
+}
+
+void sendMessage () {
+  String msg = getReadings();
+  mesh.sendBroadcast(msg);
+}
+
+//Init BME280
+void initBME(){
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }  
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+  JSONVar myObject = JSON.parse(msg.c_str());
+  int node = myObject["node"];
+  double temp = myObject["temp"];
+  double hum = myObject["hum"];
+  double pres = myObject["pres"];
+  double analog = myObject["analog"];
+  double volt = myObject["volt"];
+  Serial.print("Node: ");
+  Serial.println(node);
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.println(" %");
+  Serial.print("Pressure: ");
+  Serial.print(pres);
+  Serial.println(" hpa");
+  Serial.print("Potentiometer analog: ");
+  Serial.println(analog);
+  Serial.print("Potentiometer voltage: ");
+  Serial.println(volt);
+}
+
+
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  initBME();
+
+  mesh.setContainsRoot();
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
+}
+
+void loop() {
+  
+  mesh.update();
+
+}
